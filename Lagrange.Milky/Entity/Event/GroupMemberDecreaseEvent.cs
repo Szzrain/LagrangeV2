85d6f1745989ea7fe578e5f332780fc7dfa1660a
Szzrain <szzrain@szzrain.com>
2025-07-12 09:33:40 +0800

    [Core & Milky] Implement `group_member_decrease` Event

---
+++ Lagrange.Milky/Entity/Event/GroupMemberDecreaseEvent.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Lagrange.Milky.Entity.Event;
+
+public class GroupMemberDecreaseEvent(long time, long selfId, GroupMemberDecreaseEventData data) : EventBase<GroupMemberDecreaseEventData>(time, selfId, "group_member_decrease", data) { }
+
+public class GroupMemberDecreaseEventData
+{
+    [JsonPropertyName("group_id")]
+    public long GroupID { get; }
+    [JsonPropertyName("user_id")]
+    public long UserID { get; }
+    [JsonPropertyName("operator_id")]
+    public long? OperatorID { get; }
+
+    public GroupMemberDecreaseEventData(long groupId, long userId, long? operatorId)
+    {
+        GroupID = groupId;
+        UserID = userId;
+        OperatorID = operatorId;
+    }
+}
